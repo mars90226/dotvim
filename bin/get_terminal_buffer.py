11a424c9a0b72b245e4e53ece18471f45366af27
marspeng <marspeng@synology.com>
2020-03-06 12:04:39 +0800

    Format with black

--- bin/get_terminal_buffer.py
+++ bin/get_terminal_buffer.py
@@ -4,16 +4,21 @@ import os
 import sys
 from pynvim import attach
 
+
 def get_terminal_buffer_content(tab, win, lines):
-    nvim = attach('socket', path=os.getenv('NVIM_LISTEN_ADDRESS'))
-    win_id = nvim.call('win_getid', win, tab)
-    win_height = nvim.call('winheight', win_id)
-    buffer_handle = nvim.call('winbufnr', win_id)
-    last_lines_content = nvim.call('nvim_buf_get_lines', buffer_handle, -win_height-1, -win_height+lines-1, 0)
+    nvim = attach("socket", path=os.getenv("NVIM_LISTEN_ADDRESS"))
+    win_id = nvim.call("win_getid", win, tab)
+    win_height = nvim.call("winheight", win_id)
+    buffer_handle = nvim.call("winbufnr", win_id)
+    last_lines_content = nvim.call(
+        "nvim_buf_get_lines", buffer_handle, -win_height - 1, -win_height + lines - 1, 0
+    )
     return "\n".join(last_lines_content)
+
 
 def usage():
     print("%s: [terminal_buffer]") % sys.argv[0]
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -22,6 +27,6 @@ if __name__ == "__main__":
 
     tab = sys.argv[1]
     win = sys.argv[2]
-    lines = int(os.getenv('FZF_PREVIEW_LINES'))
+    lines = int(os.getenv("FZF_PREVIEW_LINES"))
     terminal_buffer_content = get_terminal_buffer_content(tab, win, lines)
     print(terminal_buffer_content)
