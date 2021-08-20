f586c9329c343e94379239dbdb0febc6dd4cdeff
marspeng <marspeng@synology.com>
2021-08-20 16:51:09 +0800

    fix(gitsigns.nvim): remove deprecated use_decoration_api

--- lua/vimrc/plugins/gitsigns_after.lua
+++ lua/vimrc/plugins/gitsigns_after.lua
@@ -40,7 +40,6 @@ require('gitsigns').setup {
   update_debounce = 100,
   status_formatter = nil, -- Use default
   word_diff = false,
-  use_decoration_api = true,
   use_internal_diff = true,  -- If luajit is present
   on_attach = function(bufnr)
     vim.wo.signcolumn = 'yes'
