c2ba51151672fc85ccb4ca098f3846d876586338
marspeng <marspeng@synology.com>
2021-09-23 14:41:00 +0800

    fix(gitsigns.nvim): update config to add new key mapping and fix use_internal_diff

--- lua/vimrc/plugins/gitsigns_after.lua
+++ lua/vimrc/plugins/gitsigns_after.lua
@@ -6,8 +6,10 @@ require('gitsigns').setup {
     topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
     changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
   },
+  signcolumn = true,
   numhl = false,
   linehl = false,
+  word_diff = false,
   keymaps = {
     -- Default keymap options
     noremap = true,
@@ -24,6 +26,8 @@ require('gitsigns').setup {
     ['n <Leader>hR'] = '<Cmd>lua require"gitsigns".reset_buffer()<CR>',
     ['n <Leader>hp'] = '<Cmd>lua require"gitsigns".preview_hunk()<CR>',
     ['n <Leader>hb'] = '<Cmd>lua require"gitsigns".blame_line(true)<CR>',
+    ['n <Leader>hS'] = '<Cmd>lua require"gitsigns".stage_buffer()<CR>',
+    ['n <Leader>hU'] = '<Cmd>lua require"gitsigns".reset_buffer_index()<CR>',
 
     -- Text objects
     ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
@@ -42,6 +46,7 @@ require('gitsigns').setup {
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
-  word_diff = false,
-  use_internal_diff = true,  -- If luajit is present
+  diff_opts = {
+    internal = true,
+  },
 }
