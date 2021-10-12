adc3385ac0bc41081b013ff9a17a296513792955
marspeng <marspeng@synology.com>
2021-10-12 18:34:52 +0800

    feat(packer.nvim): add utils.t() to convert termcode

--- lua/vimrc/utils.lua
+++ lua/vimrc/utils.lua
@@ -12,4 +12,12 @@ utils.get_packer_opt_dir = function()
   return utils.get_packer_dir() .. '/packer/opt'
 end
 
+-- The function is called `t` for `termcodes`.
+-- You don't have to call it that, but I find the terseness convenient
+-- ref: https://github.com/nanotee/nvim-lua-guide
+utils.t = function(str)
+    -- Adjust boolean arguments as needed
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
 return utils
