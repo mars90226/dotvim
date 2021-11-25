196733721e0c2ede98e079ec43cdc7ffc846043c
Mars Peng <mars.90226@gmail.com>
2021-11-25 22:49:16 +0800

    fix(nvim-treesitter): use better key mappings for nvim-treesitter incremental_selection

--- lua/vimrc/plugins/nvim_treesitter.lua
+++ lua/vimrc/plugins/nvim_treesitter.lua
@@ -8,10 +8,10 @@ require("nvim-treesitter.configs").setup({
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+      init_selection = "<CR>",
+      scope_incremental = "<CR>",
+      node_incremental = "<Tab>",
+      node_decremental = "<S-Tab>",
     },
   },
   indent = {
