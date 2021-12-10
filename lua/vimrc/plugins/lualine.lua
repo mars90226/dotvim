f57ae8f43ff1828bdb4240cf8d57cf1d03c89931
marspeng <marspeng@synology.com>
2021-12-10 13:15:10 +0800

    fix(statusline): use nvim_diagnostic instead of nvim_lsp

--- lua/vimrc/plugins/lualine.lua
+++ lua/vimrc/plugins/lualine.lua
@@ -8,7 +8,7 @@ require("lualine").setup({
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_lsp", "coc" } } },
+    lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_diagnostic", "coc" } } },
     lualine_c = vim.tbl_filter(function(component)
       return component ~= nil
     end, {
