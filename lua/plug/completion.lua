b5ceb005958a4fdd4079566c69be8c645a54e4f6
marspeng <marspeng@synology.com>
2021-12-14 19:03:57 +0800

    feat(LuaSnip): add snippets for vimwiki

--- lua/plug/completion.lua
+++ lua/plug/completion.lua
@@ -36,6 +36,7 @@ completion.startup = function(use)
               return vim.fn.isdirectory(path) > 0
             end, {
               utils.get_packer_start_dir() .. "/friendly-snippets",
+              vim.env.HOME .. "/.vim",
               vim.env.HOME .. "/.vim_secret",
             }),
           })
