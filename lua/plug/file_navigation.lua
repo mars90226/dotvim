4c6a7b3bd9f13555a6840022f308f5c605132b30
marspeng <marspeng@synology.com>
2022-02-14 14:01:12 +0800

    feat(harpoon): add key mapping for rm & toggle file

--- lua/plug/file_navigation.lua
+++ lua/plug/file_navigation.lua
@@ -92,6 +92,8 @@ file_navigation.startup = function(use)
       require("harpoon").setup({})
 
       nnoremap("<Leader>hm", [[<Cmd>lua require("harpoon.mark").add_file()<CR>]])
+      nnoremap("<Leader>hd", [[<Cmd>lua require("harpoon.mark").rm_file()<CR>]])
+      nnoremap("<Leader>h`", [[<Cmd>lua require("harpoon.mark").toggle_file()<CR>]])
       nnoremap("<Leader>hh", [[<Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>]])
       nnoremap("<Leader>h]", [[<Cmd>lua require("harpoon.ui").nav_next()<CR>]])
       nnoremap("<Leader>h[", [[<Cmd>lua require("harpoon.ui").nav_prev()<CR>]])
