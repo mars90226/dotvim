f125c3a5f9b0ff184285657efe5c5bec8d190f02
marspeng <marspeng@synology.com>
2022-05-18 14:40:41 +0800

    feat(repl): add iron.nvim plugin but disabled for now

--- lua/plug/terminal.lua
+++ lua/plug/terminal.lua
@@ -20,6 +20,36 @@ terminal.startup = function(use)
       vim.fn["vimrc#source"]("vimrc/plugins/neoterm.vim")
     end,
   })
+
+  use({
+    "hkupty/iron.nvim",
+    -- TODO: Seems broken in neovim 0.7? May retry it later.
+    disable = true,
+    config = function()
+      require("iron.core").setup({
+        config = {
+          -- If iron should expose `<plug>(...)` mappings for the plugins
+          should_map_plug = false,
+          -- Whether a repl should be discarded or not
+          scratch_repl = true,
+          -- Your repl definitions come here
+          repl_definition = {},
+        },
+        -- Iron doesn't set keymaps by default anymore. Set them here
+        -- or use `should_map_plug = true` and map from you vim files
+        keymaps = {
+          send_motion = "<Leader>ic",
+          visual_send = "<Leader>ic",
+          send_line = "<Leader>il",
+          repeat_cmd = "<Leader>i.",
+          cr = "<Leader>i<CR>",
+          interrupt = "<Leader>i,",
+          exit = "<Leader>iq",
+          clear = "<Leader>i<C-L>",
+        },
+      })
+    end,
+  })
 end
 
 return terminal
