f8a577d32e5e78c6a66402db1f4458713cfa624d
marspeng <marspeng@synology.com>
2022-05-18 21:54:35 +0800

    feat(neo-tree.nvim): add neo-tree.nvim plugin

--- lua/plug/file_explorer.lua
+++ lua/plug/file_explorer.lua
@@ -11,6 +11,19 @@ file_explorer.startup = function(use)
   })
   use("kristijanhusak/defx-git")
   use("kristijanhusak/defx-icons")
+
+  use({
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+    end,
+  })
 end
 
 return file_explorer
