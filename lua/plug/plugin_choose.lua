fffc3cfd6485c082d230a3f38f98a9497357025c
Mars Peng <mars.90226@gmail.com>
2022-07-10 00:38:44 +0800

    refactor(choose): move choose for plugin to plugin_choose.lua

--- lua/plug/plugin_choose.lua
+++ lua/plug/plugin_choose.lua
@@ -1,6 +1,168 @@
 local choose = require("vimrc.choose")
+local plugin_utils = require("vimrc.plugin_utils")
+local utils = require("vimrc.utils")
 
 local plugin_choose = {}
+
+plugin_choose.setup_appearance = function()
+  -- Statusline
+  -- lualine.nvim
+
+  -- Tabline
+  -- tabby.nvim, luatab.nvim, tabline.nvim, barbar.nvim, or tabline bundled in statusline
+  -- Choose tabby.nvim
+  -- TODO: Disable luatab.nvim due to not showing current tab when too many tabs
+  -- TODO: Disable tabline.nvim due to lack of normal tabline
+  -- TODO: Disable barbar.nvim due to slowness
+  -- TODO: Is fast in LunarVim, need to study why
+
+  -- Devicons
+  -- nvim-web-devicons, vim-devicons
+end
+
+plugin_choose.setup_completion = function()
+  -- Choose autocompletion plugin
+  -- nvim-cmp
+
+  -- nvim-lsp for builtin neovim lsp
+  -- builtin neovim lsp should be fast enough to be used in light vim mode
+
+  -- Choose auto pairs plugin
+  -- nvim-autopairs
+
+  -- Context in winbar
+  choose.disable_plugin("nvim-navic")
+  if vim.fn.has("nvim-0.8") == 1 and choose.is_enabled_plugin("nvim-lsp") then
+    choose.enable_plugin("nvim-navic")
+  end
+
+  -- Context in statusbar
+  if not choose.is_enabled_plugin("nvim-lsp") or choose.is_enabled_plugin("nvim-navic") then
+    choose.disable_plugin("lsp-status")
+  end
+end
+
+plugin_choose.setup_file_explorer = function()
+  -- Choose file explorer
+  -- defx.nvim
+  choose.disable_plugins({ "defx.nvim" })
+  if vim.fn["vimrc#plugin#check#python_version"]() >= "3.6.1" then
+    choose.enable_plugin("defx.nvim")
+  end
+
+  -- NOTE: Always use neo-tree.nvim
+  -- TODO: Remove others
+end
+
+plugin_choose.setup_finder = function()
+  -- Choose finder plugin
+  -- telescope.nvim
+
+  if not plugin_utils.has_linux_build_env() then
+    choose.disable_plugin("telescope-fzf-native.nvim")
+  end
+end
+
+plugin_choose.setup_git = function()
+  -- Choose between git-gutter plugin
+  -- gitsigns.nvim
+  -- TODO: Check for git version >= 2.13.0
+
+  -- Choose git-blame plugin
+  -- gitsigns.nvim
+  -- TODO: Check for git version >= 2.13.0
+end
+
+plugin_choose.setup_language = function()
+  -- Highlight {{{
+  -- nvim-treesitter for builtin neovim treesitter
+  choose.disable_plugin("nvim-treesitter")
+  if not utils.is_light_vim_mode() and plugin_utils.has_linux_build_env() then
+    choose.enable_plugin("nvim-treesitter")
+  end
+
+  -- Enable lsp-based highlighting
+  -- vim-lsp-cxx-highlight for highlighting using lsp
+  -- Do not vim-lsp-cxx-highlight when nvim-treesitter as nvim-treesitter cannot
+  -- recognize C/C++ macro semantics.
+  -- }}}
+
+  -- Lint {{{
+  -- Choose Lint plugin
+  -- null-ls.nvim
+  -- Always enable nvim-lint
+
+  -- Choose markdown-preview plugin
+  -- vim-markdown-composer, markdown-preview.nvim
+  -- TODO: Check if which plugin works
+  -- TODO: Remove vim-markdown-composer
+  choose.disable_plugins({ "vim-markdown-composer", "markdown-preview.nvim" })
+  if vim.fn["vimrc#plugin#check#has_cargo"]() then
+    choose.enable_plugin("vim-markdown-composer")
+  else
+    choose.enable_plugin("markdown-preview.nvim")
+  end
+
+  -- Enable language documentation generation
+  -- vim-doge for generating documentation
+  choose.disable_plugin("vim-doge")
+  if plugin_utils.is_executable("node") and plugin_utils.is_executable("npm") then
+    choose.enable_plugin("vim-doge")
+  end
+
+  -- Choose context plugin
+  -- nvim-treesitter-context
+  choose.disable_plugins({ "nvim-treesitter-context" })
+  if choose.is_enabled_plugin("nvim-treesitter") then
+    choose.enable_plugin("nvim-treesitter-context")
+  end
+
+  -- Context in statusline
+  if not choose.is_enabled_plugin("nvim-treesitter") or choose.is_enabled_plugin("nvim-navic") then
+    choose.disable_plugin("nvim-gps")
+  end
+end
+
+plugin_choose.setup_misc = function()
+  if vim.fn.has("python") == 0 and vim.fn.has("python3") == 0 then
+    choose.disable_plugin("vim-mundo")
+  end
+
+  -- Choose highlight plugin
+  -- builtin vim.highlight
+
+  -- Disable vim-gutentags when in nested neovim
+  if vim.fn["vimrc#plugin#check#nvim_terminal"]() == "yes" then
+    choose.disable_plugin("vim-gutentags")
+  end
+
+  if vim.fn["vimrc#plugin#check#has_browser"]() == 0 then
+    choose.disable_plugin("open-browser.vim")
+  end
+
+  -- Choose indent line plugin
+  -- indent-blankline.nvim
+
+  -- Choose window switching plugin
+  -- nvim-window
+
+  -- Choose colorizer plugin
+  -- nvim-colorizer.lua
+end
+
+plugin_choose.setup_terminal = function()
+  -- Choose terminal plugin
+  -- vim-floaterm, neoterm
+  -- Currently, always enable neoterm plugin
+end
+
+plugin_choose.setup_text_navigation = function()
+  -- Choose quick navigation plugin
+  -- hop.nvim, lightspeed.nvim
+
+  -- Choose search utility plugin
+  -- nvim-hlslens
+end
 
 plugin_choose.setup = function()
   -- plugin management
@@ -11,15 +173,15 @@ plugin_choose.setup = function()
   -- Start choosing
   choose.clear_disabled_plugins()
 
-  require("plug.choose.appearance").setup()
-  require("plug.choose.completion").setup()
-  require("plug.choose.file_explorer").setup()
-  require("plug.choose.finder").setup()
-  require("plug.choose.text_navigation").setup()
-  require("plug.choose.language").setup()
-  require("plug.choose.git").setup()
-  require("plug.choose.terminal").setup()
-  require("plug.choose.misc").setup()
+  plugin_choose.setup_appearance()
+  plugin_choose.setup_completion()
+  plugin_choose.setup_file_explorer()
+  plugin_choose.setup_finder()
+  plugin_choose.setup_text_navigation()
+  plugin_choose.setup_language()
+  plugin_choose.setup_git()
+  plugin_choose.setup_terminal()
+  plugin_choose.setup_misc()
 end
 
 return plugin_choose
