251fdd1f6447fa4ba1b42ffcbb2ff27dacee161e
Mars Peng <mars.90226@gmail.com>
2022-07-11 01:15:20 +0800

    feat(Neorg): enable completion, toc modules

--- lua/plug/utility.lua
+++ lua/plug/utility.lua
@@ -153,10 +153,28 @@ utility.startup = function(use)
         -- Tell Neorg what modules to load
         load = {
           ["core.defaults"] = {}, -- Load all the default modules
+          ["core.norg.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
+          },
           ["core.norg.concealer"] = {}, -- Allows for use of icons
           ["core.norg.dirman"] = { -- Manage your directories with Neorg
-            config = { workspaces = { my_workspace = "~/neorg" } },
+            config = {
+              workspaces = {
+                my_workspace = "~/neorg",
+                -- TODO: Wait for gtd config
+                -- gtd = "~/gtd",
+              },
+            },
           },
+          ["core.norg.qol.toc"] = {},
+          -- TODO: Need to create workspace folder first
+          -- ["core.gtd.base"] = {
+          --   config = {
+          --     workspace = "~/gtd",
+          --   },
+          -- },
         },
       })
 
