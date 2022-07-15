02865ee3e50118ffccf66735b6bfea0f48499e32
marspeng <marspeng@synology.com>
2022-07-15 10:54:53 +0800

    fix(nvim-cmp): fix nvim-cmp triggered in prompt buffer problem

--- lua/vimrc/plugins/nvim_cmp.lua
+++ lua/vimrc/plugins/nvim_cmp.lua
@@ -1,9 +1,11 @@
+local cmp_config_default = require("cmp.config.default")()
+
 local nvim_cmp = {}
 
 nvim_cmp.enabled = true
 
 nvim_cmp.is_enabled = function()
-  return nvim_cmp.enabled
+  return cmp_config_default.enabled() and nvim_cmp.enabled
 end
 
 nvim_cmp.enable = function()
