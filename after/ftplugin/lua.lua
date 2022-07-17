ce4dc5806e3e74962b406dda1e59704e59e27212
Mars Peng <mars.90226@gmail.com>
2022-07-17 15:26:32 +0800

    feat(ftplugin): set 'keywordprg' to ":help" in lua filetype

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -4,6 +4,8 @@ end
 
 vim.b.loaded_lua_settings = true
 
+vim.bo.keywordprg = ":help"
+
 nmap("<Leader>dl", [[<Plug>(Luadev-RunLine)]], "buffer")
 nmap("<Leader>dr", [[<Plug>(Luadev-Run)]], "buffer")
 nmap("<Leader>dw", [[<Plug>(Luadev-RunWord)]], "buffer")
