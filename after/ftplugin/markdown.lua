f8569228c0684a5d72f5ec64518bf2bddf06a0e0
Mars Peng <mars.90226@gmail.com>
2022-08-06 17:50:46 +0800

    feat(nvim-surround): adapt markdown surround to new api

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,17 +1,46 @@
+local surround = require("nvim-surround")
+local surround_config = require("nvim-surround.config")
+
 vim.bo.expandtab = true
 
-require("nvim-surround").buffer_setup({
-  delimiters = {
-    pairs = {
-      ["*"] = { "**", "**" },
-      ["_"] = { "__", "__" },
-      ["~"] = { "~~", "~~" },
-      ["l"] = function()
+-- NOTE: 2-characters cannot use default find & delete & change implementation
+surround.buffer_setup({
+  surrounds = {
+    ["*"] = {
+      add = { "**", "**" },
+      find = "%*%*[^*]-%*%*",
+      delete = "^(..)().-(..)()$",
+      change = {
+        target = "^(..)().-(..)()$",
+      },
+    },
+    ["_"] = {
+      add = { "__", "__" },
+      find = "__[^*]-__",
+      delete = "^(..)().-(..)()$",
+      change = {
+        target = "^(..)().-(..)()$",
+      },
+    },
+    ["~"] = {
+      add = { "~~", "~~" },
+      find = "~~[^*]-~~",
+      delete = "^(..)().-(..)()$",
+      change = {
+        target = "^(..)().-(..)()$",
+      },
+    },
+    ["l"] = {
+      add = function()
         return {
-          "[",
-          "](" .. vim.fn.getreg("*") .. ")",
+          { "[" },
+          vim.split("](" .. vim.fn.getreg("*") .. ")", "\n"),
         }
       end,
+      -- TODO: Implement find, delete, change
+      find = function() end,
+      delete = function() end,
+      change = { target = function() end },
     },
   },
 })
