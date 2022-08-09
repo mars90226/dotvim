25a37ff3b5a9b4f17d3ffa2b26b796ffa8757704
Mars Peng <mars.90226@gmail.com>
2022-08-09 23:52:42 +0800

    feat(nvim-treesitter): add highlights query for C++ & JavaScript
    1. Add highlights query for "and", "or", "not" in C++.
    2. Add highlights query for "undefined" in JavaScript

---
+++ after/queries/cpp/highlights.scm
@@ -0,0 +1,6 @@
+;; "not"
+((type_identifier) @operator (#any-of? @operator "not"))
+((identifier) @operator (#any-of? @operator "not"))
+
+;; "and", "or"
+((identifier) @operator (#any-of? @operator "and" "or"))
