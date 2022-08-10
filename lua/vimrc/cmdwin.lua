6349cd5a34c49e716ea2495114b69c41a30fd517
marspeng <marspeng@synology.com>
2022-08-10 15:06:04 +0800

    feat(nvim-surround): add vim regex boundary pairs & regex boundary pairs for cmdline-window

--- lua/vimrc/cmdwin.lua
+++ lua/vimrc/cmdwin.lua
@@ -1,8 +1,20 @@
+local surround = require("nvim-surround")
+
+local my_nvim_surround = require("vimrc.plugins.nvim_surround")
+
+
 local cmdwin = {}
 
 cmdwin.setup = function()
   -- Removing any key mapping for <CR> in cmdline-window
   nnoremap("<CR>", "<CR>", "<buffer>")
+
+  surround.buffer_setup({
+    surrounds = {
+      ["*"] = my_nvim_surround.create_all("\\<", "\\>"),
+      ["B"] = my_nvim_surround.create_all("\\b", "\\b"),
+    }
+  })
 end
 
 return cmdwin
