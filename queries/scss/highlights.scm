cdee91f40693ed03a2ec021b4fbd25cde4e0c956
marspeng <marspeng@synology.com>
2022-12-08 20:42:43 +0800

    feat(nvim-treesitter): add highlights query for scss

---
+++ queries/scss/highlights.scm
@@ -0,0 +1,11 @@
+;; extends
+
+;; @extend
+"@extend" @include
+
+;; "%placeholder"
+(placeholder_selector) @punctuation.delimiter
+(placeholder_name) @property
+
+;; $variable
+(variable_value) @variable
