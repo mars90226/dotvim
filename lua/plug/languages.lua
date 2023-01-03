e86c8a3d68c2fbe2b6617a9978fbf300ea884d9d
marspeng <marspeng@synology.com>
2023-01-03 15:30:54 +0800

    docs(hop.nvim & nvim-treesitter): add description for hopping & targeted jump mappings

--- lua/plug/languages.lua
+++ lua/plug/languages.lua
@@ -144,11 +144,13 @@ languages.startup = function(use)
         -- Ref: https://github.com/ziontee113/syntax-tree-surfer/blob/master/lua/syntax-tree-surfer/init.lua#L371-L380
         nnoremap(
           "<M-e>e",
-          [[<Cmd>lua require("syntax-tree-surfer").targeted_jump({"function", "if_statement", "else_clause", "else_statement", "elseif_statement", "for_statement", "while_statement", "switch_statement"})<CR>]]
+          [[<Cmd>lua require("syntax-tree-surfer").targeted_jump({"function", "if_statement", "else_clause", "else_statement", "elseif_statement", "for_statement", "while_statement", "switch_statement"})<CR>]],
+          { desc = "Statement targeted jump" }
         )
         nnoremap(
           "<M-e>i",
-          [[<Cmd>lua require("syntax-tree-surfer").targeted_jump({"string", "string_literal","number", "number_literal","true","false"})<CR>]]
+          [[<Cmd>lua require("syntax-tree-surfer").targeted_jump({"string", "string_literal","number", "number_literal","true","false"})<CR>]],
+          { desc = "Default targeted jump" }
         )
 
         -- filtered jump
