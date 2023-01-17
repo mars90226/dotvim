528413d9186721e5adf7557d179d5978eb3e6c08
marspeng <marspeng@synology.com>
2023-01-17 11:34:12 +0800

    feat(gitsigns): use default gitsigns signs (and support untracked files)

--- lua/vimrc/plugins/gitsigns.lua
+++ lua/vimrc/plugins/gitsigns.lua
@@ -47,13 +47,6 @@ end
 
 gitsigns.setup = function()
   require("gitsigns").setup({
-    signs = {
-      add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    },
     signcolumn = true,
     numhl = false,
     linehl = false,
