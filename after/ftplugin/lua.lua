ce234f8be3fb5ea02388369ff4ba5ac6da00d498
marspeng <marspeng@synology.com>
2023-02-23 12:11:41 +0800

    feat(nvim-surround): add lua double bracket pairs for lua

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,3 +1,7 @@
+local surround = require("nvim-surround")
+
+local my_nvim_surround = require("vimrc.plugins.nvim_surround")
+
 if vim.b.loaded_lua_settings ~= nil then
   return
 end
@@ -10,3 +14,10 @@ nmap("<Leader>dl", [[<Plug>(Luadev-RunLine)]], "buffer")
 nmap("<Leader>dr", [[<Plug>(Luadev-Run)]], "buffer")
 nmap("<Leader>dw", [[<Plug>(Luadev-RunWord)]], "buffer")
 imap("<C-G><C-D>", [[<Plug>(Luadev-Complete)]], "buffer")
+
+-- NOTE: 2-characters cannot use default find & delete & change implementation
+surround.buffer_setup({
+  surrounds = {
+    ["s"] = my_nvim_surround.create_all("[[", "]]"),
+  },
+})
