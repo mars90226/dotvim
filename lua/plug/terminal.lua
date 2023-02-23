6f4b947601f1301b7507602c441b58f435fd661d
marspeng <marspeng@synology.com>
2023-02-23 11:55:47 +0800

    refactor(packer.nvim): extract neoterm config into neoterm.lua

--- lua/plug/terminal.lua
+++ lua/plug/terminal.lua
@@ -12,28 +12,13 @@ terminal.startup = function(use)
   })
   use("voldikss/fzf-floaterm")
 
+  -- FIXME: Seems broken in neovim 0.9.0? Replace it with other terminal plugin
   use({
     "kassio/neoterm",
     cmd = { "T", "Ttoggle", "Texec" },
     keys = { "<Space>`", "<Leader>`" },
     config = function()
-      vim.g.neoterm_default_mod = "botright"
-      vim.g.neoterm_automap_keys = "<Leader>T"
-      vim.g.neoterm_size = vim.go.lines / 2
-
-      nnoremap("<Space>`", [[:execute 'T ' . input('Terminal: ', '', 'shellcmd')<CR>]], "<silent>")
-      nnoremap("<Leader>`", [[:Ttoggle<CR>]], "<silent>")
-      nnoremap("<Space><F3>", [[:TREPLSendFile<CR>]], "<silent>")
-      nnoremap("<F3>", [[:TREPLSendLine<CR>]], "<silent>")
-      xnoremap("<F3>", [[:TREPLSendSelection<CR>]], "<silent>")
-
-      -- Useful maps
-      -- hide/close terminal
-      nnoremap("<Leader>th", [[:Tclose<CR>]], "<silent>")
-      -- clear terminal
-      nnoremap("<Leader>tl", [[:Tclear<CR>]], "<silent>")
-      -- kills the current job (send a <c-c>)
-      nnoremap("<Leader>tc", [[:Tkill<CR>]], "<silent>")
+      require("vimrc.plugins.neoterm").setup()
     end,
   })
 
