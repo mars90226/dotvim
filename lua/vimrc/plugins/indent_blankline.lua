f0f2f902d893b6038223e572e9a08c5f1c76d9cb
marspeng <marspeng@synology.com>
2023-03-02 14:31:10 +0800

    perf(indent-blankeline.nvim): disable indent-blankline.nvim on certain condition
    1. Disable indent-blankline.nvim when file is too large. (> 100 KB)
    2. Disable indent-blankline.nvim show_current_context when buffer has
       more than 10000 lines.

--- lua/vimrc/plugins/indent_blankline.lua
+++ lua/vimrc/plugins/indent_blankline.lua
@@ -1,12 +1,9 @@
 local indent_blankline = {}
 
-indent_blankline.setup = function()
-  -- Currently, there's no way to differentiate tab and space.
-  -- The only way to differentiate is to disable indent-blankline.nvim
-  -- temporarily.
-  nnoremap("<Space>il", ":IndentBlanklineToggle<CR>")
-  nnoremap("<Space>ir", ":IndentBlanklineRefresh<CR>")
+indent_blankline.line_threshold = 10000
+indent_blankline.max_filesize = 100 * 1024 -- 100 KB
 
+indent_blankline.setup_config = function()
   require("indent_blankline").setup({
     char = "│",
     show_end_of_line = true,
@@ -14,18 +11,63 @@ indent_blankline.setup = function()
       "any-jump",
       "defx",
       "help",
+      "floggraph",
       "fugitive",
       "git",
       "gitcommit",
       "gitrebase",
       "gitsendemail",
+      "GV",
       "man",
       "norg",
     },
     buftype_exclude = { "nofile", "terminal" },
+    -- TODO: Try disable for performance.
+    -- indent-blankline.nvim still setup autocmd even if current buffer's 'show_current_context' is false.
+    -- Ref: https://github.com/lukas-reineke/indent-blankline.nvim/issues/440
     show_current_context = true,
     show_current_context_start = true,
   })
+end
+
+indent_blankline.setup_mapping = function()
+  -- Currently, there's no way to differentiate tab and space.
+  -- The only way to differentiate is to disable indent-blankline.nvim
+  -- temporarily.
+  nnoremap("<Space>il", ":IndentBlanklineToggle<CR>")
+  nnoremap("<Space>ir", ":IndentBlanklineRefresh<CR>")
+end
+
+indent_blankline.setup_autocmd = function()
+  local augroup_id = vim.api.nvim_create_augroup("indent_blankline_settings", {})
+  vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = augroup_id,
+    pattern = "*",
+    callback = function()
+      local line_count = vim.api.nvim_buf_line_count(vim.api.nvim_get_current_buf())
+      if line_count > indent_blankline.line_threshold then
+        vim.b.indent_blankline_show_current_context = false
+        vim.b.indent_blankline_show_current_context_start = false
+        require("indent_blankline.commands").refresh()
+      end
+    end
+  })
+  vim.api.nvim_create_autocmd({ "BufReadPre" }, {
+    group = augroup_id,
+    pattern = "*",
+    callback = function()
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
+      if ok and stats and stats.size > indent_blankline.max_filesize then
+        require("indent_blankline.commands").disable()
+      end
+    end
+  })
+end
+
+indent_blankline.setup = function()
+  indent_blankline.setup_config()
+  indent_blankline.setup_mapping()
+  indent_blankline.setup_autocmd()
 end
 
 return indent_blankline
