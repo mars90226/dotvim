7dd11e8415147aff296bd06ea15e23cf7be16570
marspeng <marspeng@synology.com>
2023-03-07 13:59:10 +0800

    fix(ftplugin): silent error when substituting pattern in todo

--- after/ftplugin/todo.lua
+++ after/ftplugin/todo.lua
@@ -6,30 +6,30 @@ vim.bo.expandtab = true
 -- Utility
 local function todo_cleanup_star()
   local save = vim.fn.winsaveview()
-  vim.cmd([=[silent %s/\* //]=])
+  vim.cmd([=[silent! %s/\* //]=])
   vim.fn.winrestview(save)
 end
 
 local function todo_set_ongoing()
   local save = vim.fn.winsaveview()
-  vim.cmd([=[silent %s/\[.\] `\[ongoing\]`/[\~]/]=])
+  vim.cmd([=[silent! %s/\[.\] `\[ongoing\]`/[\~]/]=])
   vim.fn.winrestview(save)
 end
 
 local function todo_set_done()
   local save = vim.fn.winsaveview()
-  vim.cmd([=[silent %s/\[X\]/[x]/]=])
+  vim.cmd([=[silent! %s/\[X\]/[x]/]=])
   vim.fn.winrestview(save)
 end
 
 local function todo_set_section()
   local save = vim.fn.winsaveview()
-  vim.cmd([=[silent %s/^\v\[.\] ([^:]+):/[\1]/]=])
+  vim.cmd([=[silent! %s/^\v\[.\] ([^:]+):/[\1]/]=])
   vim.fn.winrestview(save)
 end
 
 local function todo_remove_category_header()
-  vim.cmd([=[silent %global/^\v`\[(finished|ongoing|todo|postpone)\]`:$/delete]=])
+  vim.cmd([=[silent! %global/^\v`\[(finished|ongoing|todo|postpone)\]`:$/delete]=])
 end
 
 -- Functions
@@ -43,7 +43,7 @@ local function todo_format()
 end
 
 local function todo_clean()
-  vim.cmd([=[silent %global/\[x\] /delete]=])
+  vim.cmd([=[silent! %global/\[x\] /delete]=])
 end
 
 vim.api.nvim_buf_create_user_command(0, "TodoFormat", todo_format, {})
