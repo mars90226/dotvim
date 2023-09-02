9355a56344b4496a8174ece327ad5babcddfc049
Mars Peng <mars.90226@gmail.com>
2023-09-02 15:24:52 +0800

    fix(nvim-treesitter): use `attribute` instead of `meta_item`
    Ref: https://github.com/tree-sitter/tree-sitter-rust/pull/163

--- queries/rust/textobjects.scm
+++ queries/rust/textobjects.scm
@@ -4,12 +4,12 @@
 
 (array_expression (_) @swappable)
 (arguments (_) @swappable)
+(attribute) @swappable
 (enum_variant) @swappable
 (field_declaration) @swappable
 (field_initializer_list (_) @swappable)
 (function_item) @swappable
 (match_arm) @swappable
-(meta_item) @swappable
 (ordered_field_declaration_list (_) @swappable)
 (parameters (_) @swappable)
 (use_list (_) @swappable)
