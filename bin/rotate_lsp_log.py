45d1a7794e6d18c18725a93b86cd83f217cc0847
marspeng <marspeng@synology.com>
2023-09-23 11:41:58 +0800

    refactor(tool): fix lint error and add document for rotate_lsp_log.py

--- bin/rotate_lsp_log.py
+++ bin/rotate_lsp_log.py
@@ -3,18 +3,20 @@
 import glob
 import os
 import shutil
-import sys
 
 
 def get_state_dir():
+    """Return the XDG state directory, or ~/.local/state if not set."""
     return os.getenv("XDG_STATE_HOME", os.path.expanduser("~/.local/state"))
 
 
 def get_nvim_state_dir():
+    """Return the path to the nvim state directory."""
     return os.path.join(get_state_dir(), "nvim")
 
 
 def rotate_nvim_lsp_log(nvim_state_dir):
+    """Rotate the nvim lsp log files."""
     lsp_log_glob = os.path.join(nvim_state_dir, "lsp.log.*")
     current_lsp_log = os.path.join(nvim_state_dir, "lsp.log")
     rotate_lsp_log_prefix = f"{current_lsp_log}."
@@ -24,7 +26,6 @@ def rotate_nvim_lsp_log(nvim_state_dir):
         return
 
     for lsp_log in sorted(glob.glob(lsp_log_glob), reverse=True):
-        # print ext of lsp log
         lsp_log_ext = lsp_log.split(".")[-1]
         new_lsp_log = f"{rotate_lsp_log_prefix}{int(lsp_log_ext) + 1}"
         print(f"rotate {lsp_log} to {new_lsp_log}")
@@ -35,5 +36,4 @@ def rotate_nvim_lsp_log(nvim_state_dir):
 
 
 if __name__ == "__main__":
-    nvim_state_dir = get_nvim_state_dir()
-    rotate_nvim_lsp_log(nvim_state_dir)
+    rotate_nvim_lsp_log(get_nvim_state_dir())
