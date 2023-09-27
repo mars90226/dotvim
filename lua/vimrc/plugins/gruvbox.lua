55a80f0ac4d51b3dc370081fa39fb508968d9aa7
marspeng <marspeng@synology.com>
2023-09-27 13:27:53 +0800

    fix(appearance): fix custom palette for gruvbox.nvim

--- lua/vimrc/plugins/gruvbox.lua
+++ lua/vimrc/plugins/gruvbox.lua
@@ -15,44 +15,48 @@ end
 
 gruvbox.custom_overrides = function()
   local hsl = require("lush").hsl
-  local palette = require("gruvbox.palette").colors
+
+  -- TODO: Think of a better way to avoid changes needed for future gruvbox.nvim update
+  local dark_palette = require("gruvbox.palette").get_base_colors({}, "dark")
+  local light_palette = require("gruvbox.palette").get_base_colors({}, "light")
 
   local diff_percent = 20
+  -- NOTE: Assume background is dark
   local custom_palette = {
-    white_yellow = hsl(palette.bright_yellow).lighten(diff_percent * 2).hex,
-    dark_red = hsl(palette.faded_red).darken(diff_percent).hex,
-    dark_green = hsl(palette.faded_green).darken(diff_percent).hex,
-    dark_yellow = hsl(palette.faded_yellow).darken(diff_percent).hex,
-    dark_aqua = hsl(palette.faded_aqua).darken(diff_percent).hex,
+    white_yellow = hsl(dark_palette.yellow).lighten(diff_percent * 2).hex,
+    dark_red = hsl(light_palette.red).darken(diff_percent).hex,
+    dark_green = hsl(light_palette.green).darken(diff_percent).hex,
+    dark_yellow = hsl(light_palette.yellow).darken(diff_percent).hex,
+    dark_aqua = hsl(light_palette.aqua).darken(diff_percent).hex,
   }
   local overrides = {
-    NormalFloat = { fg = palette.light1, bg = palette.dark1 },
+    NormalFloat = { fg = dark_palette.fg1, bg = dark_palette.bg1 },
     TSOperator = { link = "Special" },
 
     -- NOTE: Only change background color
     DiffAdd = { fg = nil, bg = custom_palette.dark_green },
     DiffChange = { bg = custom_palette.dark_aqua },
-    DiffDelete = { fg = palette.dark0, bg = custom_palette.dark_red },
+    DiffDelete = { fg = dark_palette.bg0, bg = custom_palette.dark_red },
     DiffText = { bg = custom_palette.dark_yellow },
 
     -- NOTE: Avoid highlight link to avoid breaking tabby.nvim
-    TabLine = { fg = palette.dark1, bg = palette.dark4 },
-    TabLineFill = { fg = palette.dark4, bg = palette.dark1 },
+    TabLine = { fg = dark_palette.bg1, bg = dark_palette.bg4 },
+    TabLineFill = { fg = dark_palette.bg4, bg = dark_palette.bg1 },
 
     -- NOTE: Use similar highlight of StatusLine highlight for WinBar
     -- Color is affected by lspsaga.nvim
-    WinBar = { fg = palette.light3, bg = palette.dark0 },
-    WinBarNC = { fg = palette.light4, bg = palette.dark0 },
+    WinBar = { fg = dark_palette.fg3, bg = dark_palette.bg0 },
+    WinBarNC = { fg = dark_palette.fg4, bg = dark_palette.bg0 },
 
     -- LSP semantic highlighting
     -- Use gruvbox.nvim highlight
 
     -- Plugin
-    FocusedSymbol = { fg = palette.faded_blue, bg = custom_palette.white_yellow },
-    GitSignsCurrentLineBlame = { fg = palette.dark4 },
+    FocusedSymbol = { fg = light_palette.blue, bg = custom_palette.white_yellow },
+    GitSignsCurrentLineBlame = { fg = dark_palette.bg4 },
     -- TODO: Make nvim-ufo capture correct Folded highlight
-    UfoFoldedFg = { fg = palette.gray },
-    UfoFoldedBg = { bg = palette.dark1 },
+    UfoFoldedFg = { fg = dark_palette.gray },
+    UfoFoldedBg = { bg = dark_palette.bg1 },
   }
 
   return overrides
