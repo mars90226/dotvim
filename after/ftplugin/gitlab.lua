928f58f6cf150ca3f7fb49bb4ecbf4dc6c12421f
marspeng <marspeng@synology.com>
2024-01-16 11:58:44 +0800

    docs(gitlab.nvim): add description to key mappings for gitlab.nvim

--- after/ftplugin/gitlab.lua
+++ after/ftplugin/gitlab.lua
@@ -1,4 +1,4 @@
 local gitlab = require("vimrc.plugins.gitlab")
 
-nnoremap("gm", function() gitlab.minimize_discussion() end, "<silent>", "<buffer>")
-nnoremap("gr", function() gitlab.reset_discussion() end, "<silent>", "<buffer>")
+nnoremap("gm", function() gitlab.minimize_discussion() end, { silent = true, buffer = true, desc = "Minimize Gitlab.nvim discussion panel width" })
+nnoremap("gr", function() gitlab.reset_discussion() end, { silent = true, buffer = true, desc = "Reset Gitlab.nvim discussion panel width" })
