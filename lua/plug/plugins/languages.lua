3a4f3de636f7e0057df325482dd541df4d9554a0
marspeng <marspeng@synology.com>
2024-01-19 14:30:34 +0800

    feat(nvim-treesitter): remove playground plugin as it's archived and built-in in neovim 0.10 nightly

--- lua/plug/plugins/languages.lua
+++ lua/plug/plugins/languages.lua
@@ -54,38 +54,6 @@ local languages = {
     event = "VeryLazy",
   },
   {
-    "nvim-treesitter/playground",
-    cmd = { "TSPlaygroundToggle", "TSHighlightCapturesUnderCursor" },
-    keys = { "<Space>hp", "<Space>hh" },
-    cond = choose.is_enabled_plugin("nvim-treesitter"),
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        playground = {
-          enable = true,
-          disable = {},
-          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-          persist_queries = false, -- Whether the query persists across vim sessions
-          keybindings = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            toggle_language_display = "I",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<CR>",
-            show_help = "?",
-          },
-        },
-      })
-
-      nnoremap("<Space>hp", [[<Cmd>TSPlaygroundToggle<CR>]])
-      nnoremap("<Space>hh", [[<Cmd>TSHighlightCapturesUnderCursor<CR>]])
-    end,
-  },
-  {
     "JoosepAlviste/nvim-ts-context-commentstring",
     cond = choose.is_enabled_plugin("nvim-treesitter"),
     dependencies = { "nvim-treesitter/nvim-treesitter" },
