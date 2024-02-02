8f4fbb4b211cf3dce787309e48713217bea2ff00
marspeng <marspeng@synology.com>
2024-02-02 15:02:17 +0800

    feat(appearance): improve 'statuscolumn' options
    1. Separate gitsigns & other signs.
    2. Move fold to the rightest position.

--- lua/plug/plugins/appearance.lua
+++ lua/plug/plugins/appearance.lua
@@ -34,15 +34,42 @@ local appearance = {
       local builtin = require("statuscol.builtin")
 
       require("statuscol").setup({
+        -- Segments: (sign -> line number -> gitsigns -> fold )
+        -- Similar to Visual Studio Code
+        -- Ref: https://github.com/ofseed/nvim/blob/e0edff34c98e5ac57360273b0d0161b04fc32765/lua/plugins/ui/statuscol.lua
+        bt_ignore = { 'help', 'nofile', 'terminal' },
         segments = {
-          -- Default segments (fold -> sign -> line number -> separator)
-          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-          { text = { "%s" }, click = "v:lua.ScSa" },
+          {
+            sign = {
+              name = { ".*" },
+              text = { ".*" },
+            },
+            click = "v:lua.ScSa",
+          },
           {
             text = { builtin.lnumfunc, " " },
             condition = { true, builtin.not_empty },
             click = "v:lua.ScLa",
           },
+          {
+            sign = {
+              namespace = { "gitsigns" },
+              colwidth = 1,
+              wrap = true,
+            },
+            click = "v:lua.ScSa",
+          },
+          {
+            text = {
+              function(args)
+                args.fold.close = ""
+                args.fold.open = ""
+                args.fold.sep = "▕"
+                return builtin.foldfunc(args)
+              end,
+            },
+            click = "v:lua.ScSa",
+          }
         },
       })
 
