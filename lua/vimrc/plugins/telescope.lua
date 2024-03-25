1fbcef6bbadc49990dea71712ad4af74d2d38c29
marspeng <marspeng@synology.com>
2024-03-25 15:01:08 +0800

    feat(telescope.nvim): add telescope-fzf-native.nvim config

--- lua/vimrc/plugins/telescope.lua
+++ lua/vimrc/plugins/telescope.lua
@@ -170,6 +170,13 @@ telescope.setup_config = function()
       command_palette = command_palette_config,
       -- TODO: Disabled as telescope-frecency.nvim loading to slow
       -- frecency = frecency_config,
+      fzf = {
+        fuzzy = true,                    -- false will only do exact matching
+        override_generic_sorter = true,  -- override the generic sorter
+        override_file_sorter = true,     -- override the file sorter
+        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                         -- the default case_mode is "smart_case"
+      },
       ast_grep = {
           command = {
               "sg",
