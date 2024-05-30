9bc14debb55cbf8b768440122100298e0f98c2fc
marspeng <marspeng@synology.com>
2024-05-30 19:05:31 +0800

    feat(inspect): add :InspectEvalString to workaround limitation of `vim.fn.luaeval` that converting value from/to Lua/VimScript
    Add :InspectEvalString that use custom `luaeval()` recommended in `:help
    lua-eval` page to workaround limitation of `vim.fn.luaeval` that
    converting value from/to Lua/VimScript.

--- lua/vimrc/inspect.lua
+++ lua/vimrc/inspect.lua
@@ -1,5 +1,12 @@
 local inspect = {}
 
+-- Ref: :help lua-eval
+local chunkheader = "local _A = select(1, ...) return "
+inspect.luaeval = function(expstr, arg)
+  local chunk = assert(loadstring(chunkheader .. expstr, "luaeval"))
+  return chunk(arg) -- return typval
+end
+
 inspect.inspect = function(object, filetype)
   local ft = filetype or "lua"
   local content = vim.fn.split(vim.inspect(object), "\n")
@@ -7,6 +14,10 @@ inspect.inspect = function(object, filetype)
   vim.cmd([[new ]]..vim.fn.tempname().."."..ft)
   vim.bo.filetype = ft
   vim.api.nvim_buf_set_lines(0, 0, 0, false, content)
+end
+
+inspect.inspect_eval_string = function(string)
+  inspect.inspect(inspect.luaeval(string), "lua")
 end
 
 return inspect
