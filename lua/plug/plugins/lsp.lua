04e64ebf5971ddebb3dc9ae3583221e0d378d834
marspeng <marspeng@synology.com>
2024-07-01 12:32:52 +0800

    feat(nvim-lsp): add lazy load on commands for mason.nvim

--- lua/plug/plugins/lsp.lua
+++ lua/plug/plugins/lsp.lua
@@ -8,6 +8,14 @@ local lsp = {
     dependencies = {
       {
         "williamboman/mason.nvim",
+        cmd = {
+          "Mason",
+          "MasonUpdate",
+          "MasonInstall",
+          "MasonUninstall",
+          "MasonUninstallAll",
+          "MasonLog",
+        },
         config = function()
           require("mason").setup({
             ui = {
