e062c87756dd291275999cd0c912e837199ac00e
marspeng <marspeng@synology.com>
2024-08-06 16:45:10 +0800

    feat(lualine.nvim): add custom extension for outline.nvim

--- lua/vimrc/plugins/lualine.lua
+++ lua/vimrc/plugins/lualine.lua
@@ -4,6 +4,15 @@ local gruvbox_dark_theme = require("vimrc.plugins.lualine_theme.gruvbox_dark")
 
 local lualine = {}
 
+lualine.custom_extensions = {
+  outline = {
+    sections = {
+      lualine_a = { "filetype" },
+    },
+    filetypes = { "Outline" },
+  },
+}
+
 lualine.default_option = {
   options = {
     icons_enabled = true,
@@ -60,11 +69,11 @@ lualine.default_option = {
     lualine_y = {},
     lualine_z = {
       {
-        'windows',
-        show_filename_only = true,   -- Shows shortened relative path when set to false.
+        "windows",
+        show_filename_only = true, -- Shows shortened relative path when set to false.
         show_modified_status = true, -- Shows indicator when the window is modified.
 
-        mode = 0,                    -- 0: Shows window name
+        mode = 0, -- 0: Shows window name
         -- 1: Shows window index
         -- 2: Shows window name + window index
 
@@ -72,21 +81,21 @@ lualine.default_option = {
         -- it can also be a function that returns
         -- the value of `max_length` dynamically.
         filetype_names = {
-          TelescopePrompt = 'Telescope',
-          dashboard = 'Dashboard',
-          fzf = 'FZF',
-          alpha = 'Alpha',
-          fugitive = 'Fugitive',
-        },                                            -- Shows specific window name for that filetype ( { `filetype` = `window_name`, ... } )
+          TelescopePrompt = "Telescope",
+          dashboard = "Dashboard",
+          fzf = "FZF",
+          alpha = "Alpha",
+          fugitive = "Fugitive",
+        }, -- Shows specific window name for that filetype ( { `filetype` = `window_name`, ... } )
 
-        disabled_buftypes = { 'quickfix', 'prompt' }, -- Hide a window if its buffer's type is disabled
+        disabled_buftypes = { "quickfix", "prompt" }, -- Hide a window if its buffer's type is disabled
 
         -- windows_color = {
         --   -- Same values as the general color option can be used here.
         --   active = 'lualine_{section}_normal',     -- Color for active window.
         --   inactive = 'lualine_{section}_inactive', -- Color for inactive window.
         -- },
-      }
+      },
     },
   },
 
@@ -123,6 +132,7 @@ lualine.default_option = {
   }, "lualine.nvim-winbar") or {},
 
   extensions = {
+    -- builtin
     "fugitive",
     "fzf",
     "lazy",
@@ -133,6 +143,9 @@ lualine.default_option = {
     "overseer",
     "quickfix",
     "trouble",
+
+    -- custom
+    lualine.custom_extensions.outline,
   },
 }
 
