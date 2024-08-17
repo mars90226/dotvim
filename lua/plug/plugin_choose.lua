013b6552b42a55308aeef01f1528b32cb065c6a7
Mars Peng <mars.90226@gmail.com>
2024-08-17 17:58:13 +0800

    docs(choose): add TODO comment to check if cmp-rg should be moved to normal vim mode

--- lua/plug/plugin_choose.lua
+++ lua/plug/plugin_choose.lua
@@ -75,6 +75,7 @@ plugin_choose.setup_completion = function()
     choose.disable_plugin("cmp-git")
   end
   -- cmp-rg
+  -- TODO: Check if this should be moved to normal vim mode
   if not utils.is_main_vim_mode() or not plugin_utils.is_executable("rg") then
     choose.disable_plugin("cmp-rg")
   end
