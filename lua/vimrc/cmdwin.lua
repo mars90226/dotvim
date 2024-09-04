950ebeeec9c0ef953c5c7a73137fa33f14a8c640
marspeng <marspeng@synology.com>
2024-09-04 11:23:43 +0800

    fix(cmdwin): fix error when requiring nvim-surround in light vim mode

--- lua/vimrc/cmdwin.lua
+++ lua/vimrc/cmdwin.lua
@@ -1,22 +1,24 @@
-local surround = require("nvim-surround")
-
-local my_nvim_surround = require("vimrc.plugins.nvim_surround")
-
+local choose = require("vimrc.choose")
 
 local cmdwin = {}
 
 cmdwin.setup = function()
-  -- Removing any key mapping for special keys in cmdline-window
-  nnoremap("<CR>", "<CR>", "<buffer>")
-  nnoremap("<C-P>", "<C-P>", "<buffer>")
-  nnoremap("<C-N>", "<C-N>", "<buffer>")
+  if choose.is_enabled_plugin("nvim-surround") then
+    local surround = require("nvim-surround")
+    local my_nvim_surround = require("vimrc.plugins.nvim_surround")
 
-  surround.buffer_setup({
-    surrounds = {
-      ["*"] = my_nvim_surround.create_all([[\<]], [[\>]]),
-      ["B"] = my_nvim_surround.create_all([[\b]], [[\b]]),
-    }
-  })
+    -- Removing any key mapping for special keys in cmdline-window
+    nnoremap("<CR>", "<CR>", "<buffer>")
+    nnoremap("<C-P>", "<C-P>", "<buffer>")
+    nnoremap("<C-N>", "<C-N>", "<buffer>")
+
+    surround.buffer_setup({
+      surrounds = {
+        ["*"] = my_nvim_surround.create_all([[\<]], [[\>]]),
+        ["B"] = my_nvim_surround.create_all([[\b]], [[\b]]),
+      }
+    })
+  end
 end
 
 return cmdwin
