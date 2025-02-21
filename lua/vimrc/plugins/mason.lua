a11fedce867fed29af449eee71418fac8e4316f8
marspeng <marspeng@synology.com>
2025-02-21 14:33:37 +0800

    fix(mason): check if LSP folder exists before returing it

--- lua/vimrc/plugins/mason.lua
+++ lua/vimrc/plugins/mason.lua
@@ -8,7 +8,7 @@ mason.get_lsp_project_path = function(lsp)
   local lsp_path = mason.get_lsp_realpath(lsp)
   local lsp_folder = vim.fs.dirname(lsp_path)
   -- Check if the lsp folder is end with "/bin"
-  if lsp_folder:sub(-4) == "/bin" then
+  if lsp_folder and lsp_folder:sub(-4) == "/bin" then
     return vim.fs.dirname(lsp_folder)
   end
   return lsp_folder
