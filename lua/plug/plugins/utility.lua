4efd539153e30bcf63e7e3e20fcde90637ae65e9
marspeng <marspeng@synology.com>
2025-05-08 17:59:52 +0800

    feat(note-taking): add obsidian.nvim plugin

--- lua/plug/plugins/utility.lua
+++ lua/plug/plugins/utility.lua
@@ -120,6 +120,41 @@ local utility = {
   },
 
   {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = { "markdown" },
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see above for full list of optional dependencies ☝️
+    },
+    ---@module 'obsidian'
+    ---@type obsidian.config.ClientOpts
+    opts = {
+      -- TODO: Add secret vaults
+      workspaces = {
+        {
+          name = "notes",
+          path = "~/vaults/notes",
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
+    cmd = { "Obsidian" },
+  },
+
+  {
     "powerman/vim-plugin-AnsiEsc",
     cmd = { "AnsiEsc" },
     keys = {
