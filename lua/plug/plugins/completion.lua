a97df7e32370d9f893d02ae2033f6cb1db3de5f7
marspeng <marspeng@synology.com>
2025-06-30 18:41:51 +0800

    docs(nvim-cmp): add FIXME to find replacement for magazine.nvim plugin

--- lua/plug/plugins/completion.lua
+++ lua/plug/plugins/completion.lua
@@ -8,6 +8,7 @@ local completion = {
     -- TODO: Use 'iguanacucumber/magazine.nvim' instead of 'hrsh7th/nvim-cmp' for performance & bug
     -- fixes. Which also includes 'yioneko/nvim-cmp's performance improvements noteed in the following MR:
     -- Ref: https://github.com/hrsh7th/nvim-cmp/pull/1980
+    -- FIXME: magazine.nvim is archived...
     "iguanacucumber/magazine.nvim",
     name = "nvim-cmp", -- Otherwise highlighting gets messed up
     dependencies = vim.tbl_filter(function(plugin_spec)
