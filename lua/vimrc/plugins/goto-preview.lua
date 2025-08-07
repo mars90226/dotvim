d121d5ef3eb5e760ca272561a5130be75aceb0ce
marspeng <marspeng@synology.com>
2025-08-07 11:01:22 +0800

    fix(nvim-lsp): change key mapping for closing all windows of goto-preview

--- lua/vimrc/plugins/goto-preview.lua
+++ lua/vimrc/plugins/goto-preview.lua
@@ -2,7 +2,7 @@ local goto_preview = {
   on_attach = function(client)
     vim.keymap.set("n", "gpd", [[<Cmd>lua require('goto-preview').goto_preview_definition()<CR>]], { silent = true, buffer = true })
     vim.keymap.set("n", "gpi", [[<Cmd>lua require('goto-preview').goto_preview_implementation()<CR>]], { silent = true, buffer = true })
-    vim.keymap.set("n", "gP", [[<Cmd>lua require('goto-preview').close_all_win()<CR>]], { silent = true, buffer = true })
+    vim.keymap.set("n", "gpc", [[<Cmd>lua require('goto-preview').close_all_win()<CR>]], { silent = true, buffer = true })
     vim.keymap.set("n", "gpr", [[<Cmd>lua require('goto-preview').goto_preview_references()<CR>]], { silent = true, buffer = true })
   end,
 }
