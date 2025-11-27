ab5391eeb11e3d071310846a943845686f951ce2
marspeng <marspeng@synology.com>
2025-11-27 12:39:32 +0800

    fix(fzf-lua): add description to custom global actions

--- lua/vimrc/plugins/fzf_lua.lua
+++ lua/vimrc/plugins/fzf_lua.lua
@@ -38,12 +38,18 @@ my_fzf_lua.commands = {
 
 my_fzf_lua.global_actions = {
   -- TODO: Escape unwanted filename
-  ["alt-g"] = function(selected, opts)
-    actions.vimcmd_entry("rightbelow split", selected, opts)
-  end,
-  ["alt-v"] = function(selected, opts)
-    actions.vimcmd_entry("rightbelow vsplit", selected, opts)
-  end,
+  ["alt-g"] = {
+    fn = function(selected, opts)
+      actions.vimcmd_entry("rightbelow split", selected, opts, true)
+    end,
+    desc = "file-rightbelow-split",
+  },
+  ["alt-v"] = {
+    fn = function(selected, opts)
+      actions.vimcmd_entry("rightbelow vsplit", selected, opts, true)
+    end,
+    desc = "file-rightbelow-vsplit",
+  },
 }
 
 -- Refactor this
@@ -115,7 +121,12 @@ my_fzf_lua.setup_mapping = function()
   vim.keymap.set("n", fzf_lua_prefix .. [[h]], [[<Cmd>FzfLua help_tags<CR>]], { desc = "FzfLua help_tags" })
   vim.keymap.set("n", fzf_lua_prefix .. [[H]], [[<Cmd>FzfLua git_stash<CR>]], { desc = "FzfLua git_stash" })
   vim.keymap.set("n", fzf_lua_prefix .. [[i]], [[<Cmd>FzfLua live_grep<CR>]], { desc = "FzfLua live_grep" })
-  vim.keymap.set("n", fzf_lua_prefix .. [[I]], [[<Cmd>FzfLua live_grep_native<CR>]], { desc = "FzfLua live_grep_native" })
+  vim.keymap.set(
+    "n",
+    fzf_lua_prefix .. [[I]],
+    [[<Cmd>FzfLua live_grep_native<CR>]],
+    { desc = "FzfLua live_grep_native" }
+  )
   vim.keymap.set("n", fzf_lua_prefix .. [[j]], [[<Cmd>FzfLua jumps<CR>]], { desc = "FzfLua jumps" })
   vim.keymap.set("n", fzf_lua_prefix .. [[k]], [[<Cmd>FzfLua grep_cword<CR>]], { desc = "FzfLua grep_cword" })
   vim.keymap.set("x", fzf_lua_prefix .. [[k]], [[<Cmd>FzfLua grep_visual<CR>]], { desc = "FzfLua grep_visual" }) -- For muscle memory of grepping visual selection using fzf.vim
@@ -124,7 +135,12 @@ my_fzf_lua.setup_mapping = function()
   vim.keymap.set("n", fzf_lua_prefix .. [[L]], [[<Cmd>FzfLua lines<CR>]], { desc = "FzfLua lines" })
   vim.keymap.set("n", fzf_lua_prefix .. [[m]], [[<Cmd>FzfLua oldfiles<CR>]], { desc = "FzfLua oldfiles" })
   vim.keymap.set("n", fzf_lua_prefix .. [[p]], [[<Cmd>FzfLua tags_grep<CR>]], { desc = "FzfLua tags_grep" })
-  vim.keymap.set("x", fzf_lua_prefix .. [[p]], [[<Cmd>FzfLua tags_grep_visual<CR>]], { desc = "FzfLua tags_grep_visual" })
+  vim.keymap.set(
+    "x",
+    fzf_lua_prefix .. [[p]],
+    [[<Cmd>FzfLua tags_grep_visual<CR>]],
+    { desc = "FzfLua tags_grep_visual" }
+  )
   vim.keymap.set("n", fzf_lua_prefix .. [[P]], [[<Cmd>FzfLua tags_grep_cword<CR>]], { desc = "FzfLua tags_grep_cword" })
   vim.keymap.set("n", fzf_lua_prefix .. [[q]], [[<Cmd>FzfLua quickfix<CR>]], { desc = "FzfLua quickfix" })
   vim.keymap.set("n", fzf_lua_prefix .. [[Q]], [[<Cmd>FzfLua grep_quickfix<CR>]], { desc = "FzfLua grep_quickfix" })
@@ -136,7 +152,12 @@ my_fzf_lua.setup_mapping = function()
   vim.keymap.set("n", fzf_lua_prefix .. [[t]], [[<Cmd>FzfLua btags<CR>]], { desc = "FzfLua btags" })
   vim.keymap.set("n", fzf_lua_prefix .. [[T]], [[<Cmd>FzfLua tags<CR>]], { desc = "FzfLua tags" })
   vim.keymap.set("n", fzf_lua_prefix .. [[u]], [[<Cmd>FzfLua resume<CR>]], { desc = "FzfLua resume" })
-  vim.keymap.set("n", fzf_lua_prefix .. [[U]], [[<Cmd>FzfLua live_grep_resume<CR>]], { desc = "FzfLua live_grep_resume" })
+  vim.keymap.set(
+    "n",
+    fzf_lua_prefix .. [[U]],
+    [[<Cmd>FzfLua live_grep_resume<CR>]],
+    { desc = "FzfLua live_grep_resume" }
+  )
   vim.keymap.set("n", fzf_lua_prefix .. [[v]], [[<Cmd>FzfLua colorschemes<CR>]], { desc = "FzfLua colorschemes" })
   vim.keymap.set("n", fzf_lua_prefix .. [[w]], [[<Cmd>FzfLua tabs<CR>]], { desc = "FzfLua tabs" })
   vim.keymap.set("n", fzf_lua_prefix .. [[x]], [[<Cmd>FzfLua changes<CR>]], { desc = "FzfLua changes" })
@@ -223,17 +244,67 @@ my_fzf_lua.setup_mapping = function()
   -- Lsp
   vim.keymap.set("n", fzf_lua_lsp_prefix .. "r", "<Cmd>FzfLua lsp_references<CR>", { desc = "FzfLua lsp_references" })
   vim.keymap.set("n", fzf_lua_lsp_prefix .. "d", "<Cmd>FzfLua lsp_definitions<CR>", { desc = "FzfLua lsp_definitions" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. "D", "<Cmd>FzfLua lsp_declarations<CR>", { desc = "FzfLua lsp_declarations" })
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. "D",
+    "<Cmd>FzfLua lsp_declarations<CR>",
+    { desc = "FzfLua lsp_declarations" }
+  )
   vim.keymap.set("n", fzf_lua_lsp_prefix .. "t", "<Cmd>FzfLua lsp_typedefs<CR>", { desc = "FzfLua lsp_typedefs" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. "i", "<Cmd>FzfLua lsp_implementations<CR>", { desc = "FzfLua lsp_implementations" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. "a", "<Cmd>FzfLua lsp_code_actions<CR>", { desc = "FzfLua lsp_code_actions" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. "o", "<Cmd>FzfLua lsp_document_symbols<CR>", { desc = "FzfLua lsp_document_symbols" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. "S", "<Cmd>FzfLua lsp_workspace_symbols<CR>", { desc = "FzfLua lsp_workspace_symbols" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. "s", "<Cmd>FzfLua lsp_live_workspace_symbols<CR>", { desc = "FzfLua lsp_live_workspace_symbols" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. "c", "<Cmd>FzfLua lsp_document_diagnostics<CR>", { desc = "FzfLua lsp_document_diagnostics" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. "C", "<Cmd>FzfLua lsp_workspace_diagnostics<CR>", { desc = "FzfLua lsp_workspace_diagnostics" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. ",", "<Cmd>FzfLua lsp_incoming_calls<CR>", { desc = "FzfLua lsp_incoming_calls" })
-  vim.keymap.set("n", fzf_lua_lsp_prefix .. ".", "<Cmd>FzfLua lsp_outgoing_calls<CR>", { desc = "FzfLua lsp_outgoing_calls" })
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. "i",
+    "<Cmd>FzfLua lsp_implementations<CR>",
+    { desc = "FzfLua lsp_implementations" }
+  )
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. "a",
+    "<Cmd>FzfLua lsp_code_actions<CR>",
+    { desc = "FzfLua lsp_code_actions" }
+  )
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. "o",
+    "<Cmd>FzfLua lsp_document_symbols<CR>",
+    { desc = "FzfLua lsp_document_symbols" }
+  )
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. "S",
+    "<Cmd>FzfLua lsp_workspace_symbols<CR>",
+    { desc = "FzfLua lsp_workspace_symbols" }
+  )
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. "s",
+    "<Cmd>FzfLua lsp_live_workspace_symbols<CR>",
+    { desc = "FzfLua lsp_live_workspace_symbols" }
+  )
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. "c",
+    "<Cmd>FzfLua lsp_document_diagnostics<CR>",
+    { desc = "FzfLua lsp_document_diagnostics" }
+  )
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. "C",
+    "<Cmd>FzfLua lsp_workspace_diagnostics<CR>",
+    { desc = "FzfLua lsp_workspace_diagnostics" }
+  )
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. ",",
+    "<Cmd>FzfLua lsp_incoming_calls<CR>",
+    { desc = "FzfLua lsp_incoming_calls" }
+  )
+  vim.keymap.set(
+    "n",
+    fzf_lua_lsp_prefix .. ".",
+    "<Cmd>FzfLua lsp_outgoing_calls<CR>",
+    { desc = "FzfLua lsp_outgoing_calls" }
+  )
 
   vim.keymap.set("n", fzf_lua_lsp_prefix .. "Q", function()
     fzf_lua.lsp_workspace_symbols({ query = vim.fn.input("Query: ") })
@@ -263,38 +334,18 @@ my_fzf_lua.setup_mapping = function()
   )
 
   -- Complete
-  vim.keymap.set(
-    "i",
-    [[<C-Z><C-D>]],
-    function()
-      require("fzf-lua").complete_path()
-    end,
-    { desc = "FzfLua complete_path" }
-  )
-  vim.keymap.set(
-    "i",
-    [[<C-Z><C-F>]],
-    function()
-      require("fzf-lua").complete_file()
-    end,
-    { desc = "FzfLua complete_file" }
-  )
-  vim.keymap.set(
-    "i",
-    [[<C-Z><C-L>]],
-    function()
-      require("fzf-lua").complete_bline()
-    end,
-    { desc = "FzfLua complete_bline" }
-  )
-  vim.keymap.set(
-    "i",
-    [[<C-Z><M-l>]],
-    function()
-      require("fzf-lua").complete_line()
-    end,
-    { desc = "FzfLua complete_line" }
-  )
+  vim.keymap.set("i", [[<C-Z><C-D>]], function()
+    require("fzf-lua").complete_path()
+  end, { desc = "FzfLua complete_path" })
+  vim.keymap.set("i", [[<C-Z><C-F>]], function()
+    require("fzf-lua").complete_file()
+  end, { desc = "FzfLua complete_file" })
+  vim.keymap.set("i", [[<C-Z><C-L>]], function()
+    require("fzf-lua").complete_bline()
+  end, { desc = "FzfLua complete_bline" })
+  vim.keymap.set("i", [[<C-Z><M-l>]], function()
+    require("fzf-lua").complete_line()
+  end, { desc = "FzfLua complete_line" })
 
   -- Custom command
   vim.keymap.set("n", fzf_lua_prefix .. "<F2>", function()
