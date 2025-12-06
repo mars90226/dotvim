c37b2ca5b22c0944df47ed3404da7870766ba139
Mars Peng <mars.90226@gmail.com>
2025-12-06 22:08:52 +0800

    feat(fff.nvim): enable fff.nvim

--- lua/plug/plugins/file_navigation.lua
+++ lua/plug/plugins/file_navigation.lua
@@ -426,21 +426,40 @@ local file_navigation = {
   },
 
   -- fff.nvim
-  -- TODO: Disabled for now as UI not refreshing if keep pressing <C-J> or <C-K>
+  -- Has Typo-Resistant Fuzzy Finder
+  -- But it's slow on large folder as it need to wait for indexing
   {
     "dmtrKovalenko/fff.nvim",
-    enabled = false,
     cond = choose.is_enabled_plugin("fff.nvim"),
-    build = "cargo build --release",
-    opts = {
+    build = function()
+      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+      require("fff.download").download_or_build_binary()
+    end,
+    opts = { -- (optional)
+      -- default prompt is '🪿 '
+      prompt = "🦆 ", -- Replace the default prompt of goose emoji with duck emoji
       keymaps = {
         -- Multiple bindings supported
-        move_up = { '<Up>', '<C-P>', '<C-K>' },
-        move_down = { '<Down>', '<C-N>', '<C-J>' },
+        move_up = { "<Up>", "<C-P>", "<C-K>" },
+        move_down = { "<Down>", "<C-N>", "<C-J>" },
+      },
+      debug = {
+        enabled = true, -- we expect your collaboration at least during the beta
+        show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
       },
     },
+    -- No need to lazy-load with lazy.nvim.
+    -- This plugin initializes itself lazily.
+    lazy = false,
     keys = {
-      { "<Space>f<CR>", function() require("fff").find_files() end, desc = "FFFind FFFiles", },
+      {
+        "<Space>f<CR>",
+        function()
+          require("fff").find_files()
+        end,
+        desc = "FFFind files",
+      },
     },
   },
 
@@ -544,7 +563,7 @@ local file_navigation = {
         "<Space>gw",
         mode = { "v" },
         function()
-          require('grug-far').with_visual_selection({})
+          require("grug-far").with_visual_selection({})
         end,
         desc = "grug-far - search current word",
       },
