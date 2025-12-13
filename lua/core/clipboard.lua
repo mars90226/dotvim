3c8be31ee8eae0d99e20ca094ff76544ce86d39a
marspeng <marspeng@synology.com>
2025-12-13 23:24:47 +0800

    feat(clipboard): use tmux for clipboard if available

--- lua/core/clipboard.lua
+++ lua/core/clipboard.lua
@@ -7,6 +7,12 @@ clipboard.setup = function()
     vim.g.clipboard = 'win32yank'
   end
 
+  -- Use tmux for clipboard if available
+  -- TODO: Monitor if this cause problem
+  if vim.env.TMUX ~= nil then
+    vim.g.clipboard = 'tmux'
+  end
+
   -- TODO: Fix clipboard: error: Error: target STRING not available
   -- Use `xsel` instead of `xclip`. It seems that `xclip` over ssh with yank plugin cause this error.
   -- Ref: https://github.com/svermeulen/vim-yoink/issues/16#issuecomment-632234373
