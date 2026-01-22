3940e501c9d925457f1d06fb74b53d4fde17e3ed
marspeng <marspeng@synology.com>
2026-01-22 21:22:57 +0800

    feat(folde): use new fold unicode in Nerd Font

--- lua/vimrc/plugins/ufo.lua
+++ lua/vimrc/plugins/ufo.lua
@@ -16,7 +16,7 @@ end
 
 ufo.fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
   local newVirtText = {}
-  local suffix = ("  %d "):format(endLnum - lnum)
+  local suffix = ("  %d "):format(endLnum - lnum)
   local sufWidth = vim.fn.strdisplaywidth(suffix)
   local targetWidth = width - sufWidth
   local curWidth = 0
