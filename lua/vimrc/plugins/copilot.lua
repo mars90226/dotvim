7dec0d80821adae671453a5c5507b0fa87f61e68
marspeng <marspeng@synology.com>
2026-03-31 15:05:31 +0800

    feat(copilot): add gpt-5.4-mini

--- lua/vimrc/plugins/copilot.lua
+++ lua/vimrc/plugins/copilot.lua
@@ -7,7 +7,7 @@ my_copilot.attach_filters = {}
 my_copilot.default_model = "gpt-5.4"
 my_copilot.default_github_model = "gpt-5.4"
 my_copilot.default_free_model = "gpt-5-mini"
-my_copilot.default_fast_model = "gemini-3-flash-preview"
+my_copilot.default_fast_model = "gpt-5.4-mini"
 my_copilot.default_think_model = "claude-opus-4.6"
 
 -- NOTE: Use the model info from Copilot using CopilotChat.nvim
@@ -19,6 +19,16 @@ my_copilot.models = {
   },
   ["gpt-5.4-xhigh"] = {
     model = "gpt-5.4",
+    max_tokens = 128000,
+    reasoning_effort = "xhigh",
+  },
+  ["gpt-5.4-mini"] = {
+    model = "gpt-5.4-mini",
+    max_tokens = 128000,
+    reasoning_effort = "medium",
+  },
+  ["gpt-5.4-mini-xhigh"] = {
+    model = "gpt-5.4-mini",
     max_tokens = 128000,
     reasoning_effort = "xhigh",
   },
